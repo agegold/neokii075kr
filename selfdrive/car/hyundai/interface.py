89830e63b73032c899a0b8bc0a171f7a86e3ac20
wikarous <67613740+wikarous@users.noreply.github.com>
2020-09-23 11:54:17 +0900

    Update interface.py
    eq900, g90 조향값 튜닝

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -73,7 +73,7 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.GENESIS_G90:
       ret.mass = 2200.
       ret.wheelbase = 3.15
-      ret.steerRatio = 12.069
+      ret.steerRatio = 12.069 #참고 eq900 미국자료에는 12.9라고 나와 있음.
 
     elif candidate in [CAR.KIA_OPTIMA, CAR.KIA_OPTIMA_H]:
       ret.mass = 3558. * CV.LB_TO_KG
@@ -159,9 +159,9 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.init('lqr')
 
     ret.lateralTuning.lqr.scaleBP = [20. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS]
-    ret.lateralTuning.lqr.scaleV = [2000.0, 1700.0]
+    ret.lateralTuning.lqr.scaleV = [1700.0, 1500.0]
 
-    ret.lateralTuning.lqr.ki = 0.01
+    ret.lateralTuning.lqr.ki = 0.009
 
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
@@ -170,14 +170,14 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.lqr.l = [0.22, 0.318]
     ret.lateralTuning.lqr.dcGain = 0.003
 
-    ret.steerRatio = 14.5
-    ret.steerActuatorDelay = 0.3
-    ret.steerLimitTimer = 1.5
+    ret.steerRatio = 12.9
+    ret.steerActuatorDelay = 0.24
+    ret.steerLimitTimer = 2.5
 
-    ret.steerRateCost = 0.9
+    ret.steerRateCost = 0.5
 
     ret.steerMaxBP = [30. * CV.KPH_TO_MS, 60 * CV.KPH_TO_MS]
-    ret.steerMaxV = [1.35, 1.8]
+    ret.steerMaxV = [1.8, 2.5]
 
 
 
